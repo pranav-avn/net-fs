bcb91c3570d6c596525729abe41a393314e786ef
Pranav AVN <pranavavn@outlook.com>
2025-10-08 21:48:09 +0530

    client connect

--- nfs_client.c
+++ nfs_client.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -24,7 +25,20 @@ int main(){
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
-    inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
+    if(inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0){
+        perror("Invalid address/ Address not supported");
+        close(client_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    //connect to server
+    if(connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+        perror("Connection to server failed");
+        close(client_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connected to server %s:%d\n", SERVER_IP, SERVER_PORT);
 
     return 0;
 }
